f2697299413de5d34de1cb5e9d3354353bcdb491
Zoey Chesny <z.chesny@gmail.com>
2020-04-07 22:21:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,8 @@
+def turn()
 # ask user for their move by position 1-9
+  position = gets.strip.to_i
 # receive the user input
 # convert position to an index
 # if the move is valid, make the move and display the board to the user
 # if the move is invalid, ask for a new move until a valid move is received
+end
