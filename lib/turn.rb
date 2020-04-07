7e086606662784e5beefab592adfde262a759498
Zoey Chesny <z.chesny@gmail.com>
2020-04-07 22:21:17 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,5 @@
+# ask user for their move by position 1-9
+# receive the user input
+# convert position to an index
+# if the move is valid, make the move and display the board to the user
+# if the move is invalid, ask for a new move until a valid move is received
